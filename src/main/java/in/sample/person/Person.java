0ff4a82824b897f86cf8263ca60935039172d87b
rahul-gaidhane <rahulgaidhane98@gmail.com>
2021-07-19 18:39:58 +0530

    Add postgres database

--- src/main/java/in/sample/person/Person.java
+++ src/main/java/in/sample/person/Person.java
@@ -6,8 +6,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "person")
 public class Person {
 	
 	@Id
